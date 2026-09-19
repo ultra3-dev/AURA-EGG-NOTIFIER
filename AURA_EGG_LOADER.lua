24c0c2e5807b5edd2290b094a9dae3e24ce5f151
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-18 23:39:47 -0500

    Add small public loader

---
+++ AURA_EGG_LOADER.lua
@@ -0,0 +1,11 @@
+-- AURA EGG NOTIFIER • SMALL LOADER
+-- Pega tu webhook solo localmente. No subas el webhook a GitHub.
+
+local env = (type(getgenv) == "function" and getgenv()) or _G
+env.AURA_EGG_WEBHOOK = env.AURA_EGG_WEBHOOK or "PASTE_A_NEW_DISCORD_WEBHOOK_HERE"
+
+local source = game:HttpGet(
+    "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/main/AURA_EGG_NOTIFIER.lua"
+)
+
+loadstring(source)()
