53128132fdeb8b7f72905a35f0b58e95bc5caed0
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-22 23:07:28 -0500

    Use register-safe notifier and separate Last Seen webhook

--- AURA_EGG_LOADER.lua
+++ AURA_EGG_LOADER.lua
@@ -3,9 +3,10 @@
 
 local env = (type(getgenv) == "function" and getgenv()) or _G
 env.AURA_EGG_WEBHOOK = env.AURA_EGG_WEBHOOK or "PASTE_A_NEW_DISCORD_WEBHOOK_HERE"
+env.AURA_EGG_LAST_SEEN_WEBHOOK = env.AURA_EGG_LAST_SEEN_WEBHOOK or "PASTE_A_LAST_SEEN_DISCORD_WEBHOOK_HERE"
 
 local source = game:HttpGet(
-	"https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/main/AURA_EGG_NOTIFIER.lua?v=c35fbd7c023c4863da9764e0fd7bb02ceef468ab"
+	"https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/main/AURA_EGG_NOTIFIER.lua?v=c6b8f9e46bbecfc2f2265288086f31d6b81f2a7a"
 )
 
 local compile = loadstring or load
