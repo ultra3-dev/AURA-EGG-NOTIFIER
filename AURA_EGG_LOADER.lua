6e83a85485f64bd66e11c63c81f0367ec2dd9662
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-23 22:18:27 -0500

    release: valid immutable launcher for rarity fix

--- AURA_EGG_LOADER.lua
+++ AURA_EGG_LOADER.lua
@@ -5,7 +5,7 @@ local env = (type(getgenv) == "function" and getgenv()) or _G
 env.AURA_EGG_WEBHOOK = env.AURA_EGG_WEBHOOK or "PASTE_MAIN_DISCORD_WEBHOOK_HERE"
 env.AURA_EGG_LAST_SEEN_WEBHOOK = env.AURA_EGG_LAST_SEEN_WEBHOOK or "PASTE_LAST_SEEN_DISCORD_WEBHOOK_HERE"
 
-local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/2faffdac5b9b3b963b313f223caf4ae70ed7ffab/"
+local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/ade1b3d7931f71b5d231487a185628171712725b/"
 local files = {
 	"AURA_EGG_BOOT.lua",
 	"AURA_EGG_DETECTION.lua",
