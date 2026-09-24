09e24c3c24fa7d78341c9d27883a5c3a042b04a9
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-23 21:31:44 -0500

    fix: pin loader to webhook delivery repair

--- AURA_EGG_LOADER.lua
+++ AURA_EGG_LOADER.lua
@@ -5,7 +5,7 @@ local env = (type(getgenv) == "function" and getgenv()) or _G
 env.AURA_EGG_WEBHOOK = env.AURA_EGG_WEBHOOK or "PASTE_MAIN_DISCORD_WEBHOOK_HERE"
 env.AURA_EGG_LAST_SEEN_WEBHOOK = env.AURA_EGG_LAST_SEEN_WEBHOOK or "PASTE_LAST_SEEN_DISCORD_WEBHOOK_HERE"
 
-local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/276c05b0753cf17dc5dd9d7a87a26a3899694a8e/"
+local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/e39873aa494872e4856269b5a490e36c135a4742/"
 local files = {
 	"AURA_EGG_BOOT.lua",
 	"AURA_EGG_DETECTION.lua",
