7df781d41ecf9195307968fb63d03f53832aa684
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-25 19:34:53 -0500

    Fix notifier compile error and update UI: AURA_EGG_LOADER.lua

--- AURA_EGG_LOADER.lua
+++ AURA_EGG_LOADER.lua
@@ -5,7 +5,7 @@ local env = (type(getgenv) == "function" and getgenv()) or _G
 env.AURA_EGG_WEBHOOK = env.AURA_EGG_WEBHOOK or "PASTE_MAIN_DISCORD_WEBHOOK_HERE"
 env.AURA_EGG_LAST_SEEN_WEBHOOK = env.AURA_EGG_LAST_SEEN_WEBHOOK or "PASTE_LAST_SEEN_DISCORD_WEBHOOK_HERE"
 
-local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/4388d21676ece10f6b9bfc9e51be9270fd283db5/"
+local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/main/"
 local files = {
 	"AURA_EGG_BOOT.lua",
 	"AURA_EGG_DETECTION.lua",
