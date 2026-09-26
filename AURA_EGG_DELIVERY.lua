97748fcdea33360e8fabe56dcabbe70edf2ae765
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-26 08:35:55 -0500

    Restore AURA Egg Notifier 2.2.8 stable release

--- AURA_EGG_DELIVERY.lua
+++ AURA_EGG_DELIVERY.lua
@@ -1,4 +1,4 @@
--- AURA EGG 2.3.0 ULTRA // DELIVERY + EVENT LOOP
+-- AURA EGG 2.2.8 ULTRA // DELIVERY + EVENT LOOP
 local function sendEggAlert(description, sourceText, onDone)
 	task.spawn(function()
 		local payload = {
@@ -342,3 +342,7 @@ appendConsoleEntry("AURA EGG NOTIFIER ACTIVATED", "MessageInfo", "SCRIPT")
 updateStatus("SYSTEM VERSION: " .. CONFIG.Version .. " // READY", Color3.fromRGB(99, 255, 154))
 scheduleLastSeenUpdate()
 print("AURA EGG NOTIFIER ACTIVATED")
+end
+
+
+runNotifierRuntime()
