14b2e0395355f9483f4c4d5556301790adf2a140
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-26 07:00:05 -0500

    chore: pin loader to panel fix

--- AURA_EGG_LOADER.lua
+++ AURA_EGG_LOADER.lua
@@ -5,7 +5,7 @@ local env = (type(getgenv) == "function" and getgenv()) or _G
 env.AURA_EGG_WEBHOOK = env.AURA_EGG_WEBHOOK or "PASTE_MAIN_DISCORD_WEBHOOK_HERE"
 env.AURA_EGG_LAST_SEEN_WEBHOOK = env.AURA_EGG_LAST_SEEN_WEBHOOK or "PASTE_LAST_SEEN_DISCORD_WEBHOOK_HERE"
 
-local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/4d14f5d02f5a85f614cfcc56fde09c00e32d22fc/"
+local base = "https://raw.githubusercontent.com/ultra3-dev/AURA-EGG-NOTIFIER/589262806eec4f5deae956cbb1a7e03d31d44f23/"
 local files = {
 	"AURA_EGG_BOOT.lua",
 	"AURA_EGG_DETECTION.lua",
