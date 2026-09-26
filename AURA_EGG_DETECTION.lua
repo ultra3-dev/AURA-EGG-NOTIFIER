97fcadce6450177e854f3fabdd599f5403dadb74
ULTRA3_DEV <yeanfranaparicio@gmail.com>
2026-09-26 08:03:46 -0500

    Release AURA Egg Notifier 2.3.0

--- AURA_EGG_DETECTION.lua
+++ AURA_EGG_DETECTION.lua
@@ -1,6 +1,5 @@
--- AURA EGG 2.2.9 ULTRA // DETECTION + CATALOG
+-- AURA EGG 2.3.0 ULTRA // DETECTION + CATALOG
 -- Rango de mayor a menor rareza. Los empates conservan el orden detectado.
-local function runNotifierRuntime()
 local RARITY_ORDER = {
 	{keyword = "divine", rank = 1, roleId = "1544734510665699389"},
 	{keyword = "eternal", rank = 2, roleId = "1544734452054229173"},
